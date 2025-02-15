486814a1c62da3dc91010e7209ec182cad17f57e
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2025-02-15 09:01:13 +0000

    updated

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
+import re
 import whisper
 
+# Load Whisper model
 model = whisper.load_model("turbo")
-result = model.transcribe("captcha_0001.wav")
-print(result["text"])
+
+# Transcribe the audio captcha
+result = model.transcribe("captcha_0003.wav", fp16=False)
+
+# Extract letters before commas and periods
+captcha_text = "".join(re.findall(r'(\w)(?=[,.])', result["text"]))
+
+print("Transcribed Text:",result["text"])
+print("Formatted Captcha:", captcha_text)
